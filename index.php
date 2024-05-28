7ffb569999d69d7fc7dcfb004aa2e3b2379e0409
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-05-28 16:15:26 -0300

    up

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="inde.css">
+  <title>Up-load</title>
+</head>
+
+<body>
+
+
+
+
+
+
+
+  <div class="perfil">
+
+    <form action="up.php" method="post" enctype="multipart/form-data">
+      Selecione Imagem<br>
+      <input type="file" name="arquivo"><br>
+      <input type="submit" value="enviar" name="submit">
+    </form>
+
+  </div>
+
+
+
+
+
+  </main>
+</body>
+</html>
